4ce4f2dbcc1b1960a20f2856bcee5f186b72ee30
Jonathan Debétaz <contact@jonathan-d.ch>
2022-03-24 21:35:17 +0100

    Some update

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,17 @@
 import '../scss/app.scss'
+feather.replace()
 
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+document.getElementById("download").addEventListener("click", function(){
+    var filename = "hello.txt";
+    download(filename, "Je suis un test");
+}, false);
